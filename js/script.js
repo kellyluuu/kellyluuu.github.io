23ddcd529abf54e360defbdde0bb0e29f9bd9966
KELLY LUU <kellyluuu@gmail.com>
2022-03-21 02:18:50 -0500

    update css and js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const api = '2b131e287e23c9c16111592776823fdb'
-const $container = $('container')
+const $container = $("#container")
 const $city = $('#city')
 const $feel = $('#feel')
 const $weather = $('#weather')
 const $temp = $("#temperature")
-const $button = $("button")
+const $button = $("#search-button")
 const $input = $("input")
 const $aside = $("aside")
 const $length = $("#length")
-const $bottomDiv = $("four")
+const $one =$(".one")
+const $five= $(".five")
 const $ul = $("ul")
 
 function day (x){
@@ -19,45 +20,13 @@ function hr (x){
     const dateObject = new Date(x*1000)
     return dateObject.toLocaleString("en-US", {hour: "numeric"}) 
 }
-
-function reset (){
-    $container.html(`        <div class="one">
-    <h1>AJAX Weather App</h1>
-    <input type="text" placeholder="City"/>
-    <button>Get Weather</button>
-    <form action="format">
-        <label for="length">
-        <select name="length" id="length" required>
-          <option value="now">Current Forcast</option>
-          <option value="24hr">24 hours Forcast</option>
-          <option value="5d">5 Days Forcast</option>
-          <option value="7d">7 Days Forcast</option>
-        </label>
-        </select><a> RANGE</a>
-      </form>
-</div>
-<div class="two">   
-    <p><b>LOCATION:</b><span id="city">  </span></p>
-    <p><b>TEMPERATURE:</b><span id="temperature">  </span></p>
-
-</div>
-<div class="three">
-    <p><b>FEELS-LIKE:</b><span id="feel"></span>  </p>
-    <p><b>WEATHER: </b><span id="weather"></span>  </p>  
-
-</div>
-<div class="four">
-    <aside>
-        <ul></ul>
-    </aside>
-</div>
-</div>`)
-}
+$five.hide()
 
 $button.on("click", (event) => {
     event.preventDefault()
-    reset()
-    const searchTerm = $input.val()
+    const searchTerm = $input.val();
+    $one.hide()
+    $five.show()
     $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${api}&units=imperial`)
         .then((data) => {
             const lat = JSON.stringify(data.coord.lat)
@@ -67,17 +36,18 @@ $button.on("click", (event) => {
             $feel.text (` ${data.main.feels_like} \xB0 F`)
             const descript = data.weather[0].description
             $weather.text(` ${descript}`)
+            $five.text(`${data.name.toUpperCase()}`)
             $.ajax(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${api}&units=imperial`)
             .then((data2)=>{
                 if ($length.val()==='now'){
-                    $aside.html(`<h2>${day(data2.daily[0].dt)} : ${data2.current.temp} \xB0 F <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></h2><h2>${descript.toUpperCase()}</h2>`)
+                    $aside.html(`<h4>${day(data2.daily[0].dt)} : ${data2.current.temp} \xB0 F <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></h4><h4>${descript.toUpperCase()}</h>`)
                 }else if ($length.val()==='5d'){   
                     for (let i=0; i<5; i++){
                         const $li = $("<li>")
                         $li.html(`<h4>${day(data2.daily[i].dt)} Temp: ${data2.daily[i].temp.day} \xB0 F 
                         <img src="http://openweathermap.org/img/wn/${data2.daily[i].weather[0].icon}@2x.png">${data2.daily[i].weather[0].description}</h4>`)
                         $aside.append($li)
-                    }  
+                    }     
                 }else if ($length.val()==='7d'){
                     for (let i=0; i<7; i++){
                         const $li = $("<li>")
@@ -88,8 +58,8 @@ $button.on("click", (event) => {
                 }else if ($length.val()==='24hr'){
                     for (let i=0; i<24; i++){
                         const $li = $("<li>")
-                        $li.html(`<h4>${hr(data2.hourly[i].dt)} Temp: ${data2.hourly[i].temp} \xB0 F 
-                        <img src="http://openweathermap.org/img/wn/${data2.hourly[i].weather[0].icon}@2x.png">${data2.hourly[i].weather[0].description}</h4>`)
+                        $li.html(`<p>${hr(data2.hourly[i].dt)} Temp: ${data2.hourly[i].temp} \xB0 F 
+                        <img src="http://openweathermap.org/img/wn/${data2.hourly[i].weather[0].icon}@2x.png" inline-size="10%">${data2.hourly[i].weather[0].description}</p>`)
                         $aside.append($li)
                     }  
                 }}
